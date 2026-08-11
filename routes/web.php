af61e5879ef36ea87ad763c7b784f5b8255e6d22
OctoY <yann@octoplus.solutions>
2026-08-11 10:40:47 +0200

    Add SitemapController and XML views for sitemap generation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\SitemapController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/sitemap.xml', [SitemapController::class, 'index'])
+    ->name('sitemap.index');
+
+Route::get('/sitemap-{locale}.xml', [SitemapController::class, 'locale'])
+    ->name('sitemap.locale');
 
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
